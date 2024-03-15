e4cfca52b79e5cc789a339972029c85216ac53e1
immi <mimranabid2@gmail.com>
2024-03-15 18:00:44 +0500

    Home table heading
    and also remove unnecessary things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,77 +18,15 @@ import {
 } from "@/components/ui/table"
 import { useRouter } from 'next/navigation';
 
-function rowClassName(even:boolean, isCurrent:boolean=false){
-  return clsx("rowShadow TRow my-4", isCurrent && "")
-}
 
-function Home(){
-  const headers = ["", "Date", "Day", "Sehar", "Iftar"]
-  return (
-    <WrapperDiv className='min-w-fit w-3/4'>
-    <div className="w-full">
-      <table className="table-auto box-border w-full">
-        <thead className=''>
-          <tr className='rounded-lg rowShadow TRow'>
-            {headers.map((header, index) => (
-              <th key={index} className="">
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <br className='leading-[0em] block' />
-        <tbody>
-          {ramadanDates.map((robj, rowIndex) => (
-            <Row key={rowIndex} rObj={robj} className={rowClassName(rowIndex%2==0)}/>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    </WrapperDiv>
-  );
-};
-
-function Row({rObj, className=""}:{rObj:ramadanDateObj, className?:string}){
-  return (
-    <tr className={className}>
-      <td>
-        {zfill(rObj.ramadanNo)}
-      </td>
-      <td>
-        {formatDate(rObj.seharDate, "dd-LLL")}
-      </td>
-      <td>
-        {formatDate(rObj.seharDate, "eeee")}
-      </td>
-      <td>
-        {formatDate(rObj.seharDate, "hh:mm")}
-      </td>
-      <td>
-        {formatDate(rObj.iftarDate, "hh:mm")}
-      </td>
-    </tr>
-  )
-}
-
-function HomeOld() {
-  return (
-
-    <ol className="lg:grid-rows-5 md:grid-rows-7 sm:grid-rows-10 grid-flow-row grid sm:grid-flow-col items-center content-center justify-center lg:h-40 gap-x-10 gap-y-1 mb-5">
-      {ramadanDates.map((obj)=>(
-        <li key={obj.ramadanNo} className="text-nowrap">🕋 <Link  href={`/${obj.ramadanNo}`} className="hover:underline text-[#ffb02e]"> Ramadan {obj.ramadanNo}
-          </Link>
-        </li>
-      ))}
-    </ol>
-  );
-}
-
-export default function TableDemo() {
+export default function Home() {
   const currentDate = new Date()
 
   return (
-    <WrapperDiv className='min-w-fit w-3/4'>
+
+    <div className='sm:mt-16 mt-0 w-full flex flex-col items-center'>
+      <h1 className='text-center mb-5 sm:mt-5 gradient-text text-4xl font-extrabold'>Ramadan Calendar <br /> 2024</h1>
+    <WrapperDiv className='min-w-fit w-3/4 sm:mt-0'>
     <div className='overflow-x-scroll'>
     <Table className='table-auto'>
       <TableCaption className='text-border'>According to the Greenwich Mean Time (GMT) set on your device.</TableCaption>
@@ -110,6 +48,7 @@ export default function TableDemo() {
     </Table>
     </div>
     </WrapperDiv>
+    </div>
 
   )
 }
