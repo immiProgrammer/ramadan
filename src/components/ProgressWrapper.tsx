e7ecaf238c2f03d1ffac6cc7522c8115f83a3e81
immi <mimranabid2@gmail.com>
2024-03-18 12:43:02 +0500

    Update ProgressWrapper.tsx with suspense

--- src/components/ProgressWrapper.tsx
+++ src/components/ProgressWrapper.tsx
@@ -1,17 +1,18 @@
-'use client';
-import { ProgressBar, Link } from '@lexz451/next-nprogress';
-
+import { ProgressBar } from '@lexz451/next-nprogress';
+import {Suspense} from "react";
 const ProgressWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-    <ProgressBar
-      color='hsl(var(--popover-foreground))'
-      height='2px'
-      options={{
-        showSpinner:false
-      }}
-      />
-        {children}
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProgressBar
+          color='hsl(var(--popover-foreground))'
+          height='2px'
+          options={{
+            showSpinner:false
+          }}
+          />
+      </Suspense>
+      {children}
     </>
   );
 };
